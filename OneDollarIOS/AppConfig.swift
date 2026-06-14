7b9a8c3205a9ff47815e45e381f64d502464e0ce
s1214098-ship-it <s1214098@gmail.com>
2026-06-15 05:37:52 +0800

    Add iOS project files

---
+++ OneDollarIOS/AppConfig.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum AppConfig {
+    static let appName = "海賊團一元競標"
+
+    // App Store build URL. Source found in /Volumes/Web/ONE DOLLOR/public/index.html.
+    static let webAppURL = URL(string: "https://www.s1214098.com.tw/ONE%20DOLLOR/")!
+}
